e9b9fd4e8e16e263740fc109ad74ee339b28d23e
ianjakobs <92268919+ianjakobs@users.noreply.github.com>
2021-10-12 12:41:20 +0200

    Set up Fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,32 @@
+import * as Fathom from 'fathom-client'
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 import '@/styles/global.css'
 
-const App = ({ Component, pageProps }: AppProps) => (
-    <Component {...pageProps} />
-)
+const App = ({ Component, pageProps }: AppProps) => {
+    const router = useRouter()
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'production') {
+            Fathom.load(process.env.NEXT_PUBLIC_FATHOM_ID, {
+                includedDomains: ['ianjakobs.com'],
+                url: `https://${process.env.NEXT_PUBLIC_FATHOM_DOMAIN}/script.js`,
+            })
+        }
+
+        const onRouteChangeComplete = () => Fathom.trackPageview()
+        router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+        return () => {
+            router.events.off('routeChangeComplete', onRouteChangeComplete)
+        }
+    }, [router.events])
+
+    return (
+        <Component {...pageProps} />
+    )
+}
 
 export default App
