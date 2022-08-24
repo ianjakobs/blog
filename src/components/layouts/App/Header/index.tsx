71c2c458357c63eeda9d86fab7b76c5c73207b41
ianjakobs <92268919+ianjakobs@users.noreply.github.com>
2022-08-24 14:25:54 +0200

    Add the size of tappable elements for mobile

--- src/components/layouts/App/Header/index.tsx
+++ src/components/layouts/App/Header/index.tsx
@@ -3,18 +3,18 @@ import { Container, Icon } from '@/components/ui'
 import { TWITTER } from '@/lib/constants'
 
 const Header = () => (
-    <Container as="header" className="flex pt-4 sm:pt-8 pb-12 sm:pb-24">
-        <nav className="space-x-6">
+    <Container as="header" className="flex pt-1 sm:pt-3 pb-12 sm:pb-24">
+        <nav className="flex space-x-8">
             <Link href="/">Home</Link>
             {/* <Link href="/posts">Posts</Link> */}
             <Link href="/about">About</Link>
         </nav>
 
-        <Link aria-label="Twitter" className="ml-auto" href={TWITTER} variant="twitter">
+        <Link aria-label="Twitter" className="ml-auto px-4" href={TWITTER} variant="twitter">
             <Icon className="inline fill-current" name="twitter" />
         </Link>
 
-        <Link aria-label="RSS" className="ml-3" href="/feed.xml" variant="rss">
+        <Link aria-label="RSS" className="-mr-4 px-4" href="/feed.xml" variant="rss">
             <Icon className="inline" name="rss" />
         </Link>
     </Container>
